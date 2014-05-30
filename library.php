020e7ae86643d5cfeb6f76d68f24e8f3b11ee99b
abhinay302 <abhinay302@gmail.com>
2014-05-30 21:48:57 +0530

    FB_login

---
+++ library.php
@@ -0,0 +1,4 @@
+<?php
+mysql_connect("localhost", "root", "123") or die ("Oops! Server not connected"); // Connect to the host
+mysql_select_db("demo") or die ("Oops! DB not connected"); // select the database
+?>
